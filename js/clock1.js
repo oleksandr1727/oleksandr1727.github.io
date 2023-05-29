4fade86c74496beeb0bebe1e8ea3dd69167d7779
VXrch <nika23888@gmail.com>
2023-05-29 16:53:12 +0300

    Виправленя помилка з годиником

--- js/clock1.js
+++ js/clock1.js
@@ -1,11 +1,12 @@
-function clockPainting() {
+function clockPainting() 
+{
     var now = new Date();
     var sec = now.getSeconds();
     var min = now.getMinutes();
     var hr = now.getHours();
   
     var ctx = document.getElementById("canvas").getContext("2d");
-    ctx.save();// помещаем текущий контекст в стэк
+    ctx.save();
   
     ctx.clearRect(0,0,150,150);
     ctx.translate(75, 75);
@@ -26,11 +27,11 @@ function clockPainting() {
         ctx.lineTo(120,0);
     }
   
-    ctx.stroke();// нарисовали то, что ранее описали
-    ctx.restore();// достаем последний сохраненный контекст из стэка
+    ctx.stroke();
+    ctx.restore();
   
     ctx.save();
-    // рисуем часовую стрелку, вращая холст
+    
     ctx.rotate((Math.PI/6)*hr +
         (Math.PI/360)*min +
         (Math.PI/21600)*sec);
@@ -39,14 +40,14 @@ function clockPainting() {
     ctx.beginPath();
     ctx.moveTo(-20,0);
   
-    // линия почти до часовых меток
+    
     ctx.lineTo(80,0);
     ctx.stroke();
     ctx.restore();
   
     ctx.save();
   
-    // минутная стрелка
+    
     ctx.rotate((Math.PI/30*min) +
         (Math.PI/1800)*sec);
     ctx.lineWidth = 10;
@@ -59,9 +60,9 @@ function clockPainting() {
   
     ctx.save();
   
-    // секундная стрелка
+    
     ctx.rotate(sec * Math.PI/30);
-    ctx.strokeStyle = "#D40000";// цвет контура
+    ctx.strokeStyle = "#D40000";
     ctx.fillStyle = "#D40000";
     ctx.lineWidth = 6;
   
